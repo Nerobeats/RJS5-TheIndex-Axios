6730ff08ee6c0ed5b100c01142a7372706452045
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-03-16 15:44:08 +0200

    mshary's wisdom is applied

--- src/BookRow.js
+++ src/BookRow.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const BookRow = props => {
   const book = props.book;
-  let authorName = "";
-  for (var i = 0; i < book.authors.length; i++) {
-    if (i !== book.authors.length - 1) {
-      authorName += book.authors[i].name + " ,";
-    } else {
-      authorName += book.authors[i].name;
-    }
-  }
+  // let authorName = "";
+  // for (var i = 0; i < book.authors.length; i++) {
+  //   if (i !== book.authors.length - 1) {
+  //     authorName += book.authors[i].name + " ,";
+  //   } else {
+  //     authorName += book.authors[i].name;
+  //   }
+  // }
 
   return (
     <tr>
       <td>{book.title}</td>
-      <td>{authorName}</td>
+      <td>{book.authors.map(author => author.name).join(" ,")}</td>
       <td>
         <button className="btn" style={{ backgroundColor: book.color }} />
       </td>
